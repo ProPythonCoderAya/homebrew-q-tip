dec666041a8b9fa0e58b989024611845073cfda6
Ayaan Banday <nebulaos.com@gmail.com>
2026-08-29 20:08:35 +0200

    a

--- Formula/q-tip.rb
+++ Formula/q-tip.rb
@@ -1,8 +1,8 @@
 class QTip < Formula
   desc "A simple C++ graphics framework"
   homepage "https://github.com/ProPythonCoderAya/Q-Tip"
-  url "https://github.com/ProPythonCoderAya/Q-Tip/archive/refs/tags/DEBUG.tar.gz"
-  sha256 "e6e33f8786a4d1125718dac3ea8f90c48653c3a10093d81b4fb4708576315686"
+  url "https://github.com/ProPythonCoderAya/Q-Tip/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "3615a28d8c27a8960d8c5cd70e09d0bda296c13af9468c775b55c97ad73b8006"
   license "MIT"
 
   depends_on "cmake"
