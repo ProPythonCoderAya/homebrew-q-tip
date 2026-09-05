baa4cd32769827cca51629a98e93bcdfdc45171e
Ayaan Banday <nebulaos.com@gmail.com>
2026-09-05 09:30:53 +0200

    Update Q-Tip to v0.1.5

--- Formula/q-tip.rb
+++ Formula/q-tip.rb
@@ -1,8 +1,8 @@
 class QTip < Formula
   desc "A simple C++ graphics framework"
   homepage "https://github.com/ProPythonCoderAya/Q-Tip"
-  url "https://github.com/ProPythonCoderAya/Q-Tip/archive/refs/tags/v0.1.5.tar.gz"
-  sha256 "38380fb46d64ef920db6d7921f11d5b9592b4e56a4a72c11177ffad68bd564e0"
+  url "https://github.com/ProPythonCoderAya/Q-Tip/archive/refs/tags/v0.1.5.tar.gz.tar.gz"
+  sha256 "7e99b1aec9e1a9334c4bd4aba6519abed61c9627881f4faeeb2a9992725f3fe8"
   license "MIT"
 
   depends_on "cmake"
