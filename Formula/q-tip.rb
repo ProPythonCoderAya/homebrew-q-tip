5bdc163583f21d9ca63bc95ade9563ee2a8ea3f2
Ayaan Banday <nebulaos.com@gmail.com>
2026-09-12 18:18:54 +0200

    a

--- Formula/q-tip.rb
+++ Formula/q-tip.rb
@@ -3,7 +3,7 @@ class QTip < Formula
   homepage "https://github.com/ProPythonCoderAya/Q-Tip"
   url "https://github.com/ProPythonCoderAya/Q-Tip/archive/refs/tags/v0.1.6.tar.gz"
   head "https://github.com/ProPythonCoderAya/Q-Tip.git", branch: "main"
-  sha256 "146b31cf7a3b992f9ad667fc4068b58a0fb7f1fe920de23164a387e63d241a6a"
+  sha256 "13fbf189263ab426bb308c8983e3d8249002b104c6901837f1207576e6073cff"
   license "MIT"
 
   depends_on "cmake"
